1e4e65e8f4162ae583d9e2deb99f8f696a32cb70
landers <liaorenj@gmail.com>
2023-07-29 19:25:16 +0800

    修改help关于为fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 
 function Home(props) {
   const [count, setCount] = useState(0)
+  const [openAbout, setOpenAbout] = useState(false);
+
   const getPhotosCount = () => {
     fetch("/photos.json", {cache: 'no-cache'}).then(r => {
       r.json().then(res => {
@@ -34,6 +36,15 @@ function Home(props) {
         <p className="read-the-docs">
           Get source code on <a href="https://github.com/JJApplication/Palace">github</a>
         </p>
+        <div className="about" onClick={() => setOpenAbout(!openAbout)}>
+          ?
+        </div>
+        {openAbout &&
+            <div className="about-content">
+              Hello, 这是一个在线相册项目, 用来记录生活<br/>
+              项目基于React-Album开发, 持续改进中...<br/>
+              你可以fork<a href="https://github.com/JJApplication/Palace">本项目</a>并二次开发, Enjoy today ~ <br/>
+            </div>}
       </>
   )
 }
